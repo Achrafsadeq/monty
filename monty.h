2cb3085a9df7071069d93f3aae8f4eb07ff15351
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-26 10:15:36 -0700

    All prototypes must be included in the header file monty.h

--- monty.h
+++ monty.h
@@ -42,40 +42,30 @@ typedef struct instruction_s
 extern stack_t *head;
 typedef void (*op_func)(stack_t **, unsigned int);
 
-/* File operations */
 void open_file(const char *file_name);
 int manage_line_parsing(char *buffer, int line_number, int format);
 void manage_file_read(FILE *);
 int manage_char_count(FILE *);
 void find_func(char *, char *, int, int);
 
-/* Stack operations */
 stack_t *create_node(int n);
 void free_nodes(void);
 void display_stack(stack_t **, unsigned int);
 void push_stack(stack_t **, unsigned int);
 void add_to_queue(stack_t **, unsigned int);
-
 void execute_func(op_func, char *, char *, int, int);
-
 void print_top_node(stack_t **, unsigned int);
 void remove_top(stack_t **, unsigned int);
 void do_nothing(stack_t **, unsigned int);
 void swap_elements(stack_t **, unsigned int);
-
-/* Math operations with nodes */
 void add_nodes(stack_t **, unsigned int);
 void subtract_nodes(stack_t **, unsigned int);
 void divide_nodes(stack_t **, unsigned int);
 void multiply_nodes(stack_t **, unsigned int);
 void modulate_nodes(stack_t **, unsigned int);
-
-/* String operations */
 void print_character(stack_t **, unsigned int);
 void print_string(stack_t **, unsigned int);
 void rotl(stack_t **, unsigned int);
-
-/* Error handling */
 void error(int error_code, ...);
 void extended_error(int error_code, ...);
 void string_error(int error_code, ...);
